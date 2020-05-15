dce14ba651a2c055bfbabdb75ba224758cccc420
c1phe4 <c1phe4@protonmail.com>
2020-05-15 19:08:57 +0530

    Atom Text Editor install script for Debian Sys

---
+++ atom.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#add our official package repository to your system
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt-get update
+
+# Install Atom
+sudo apt-get install atom
+# Install Atom Beta
+sudo apt-get install atom-beta
+
+
